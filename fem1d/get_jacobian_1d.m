4eb09e96eda03a8f97a3c07b08e13bd6b05bdfef
June 雲如 <sy1162@nyu.edu>
2021-04-29 11:50:52 +0200

    Delete fem1d directory

--- fem1d/get_jacobian_1d.m
+++
@@ -1,7 +0,0 @@
-function [jacobi_mat, jacobian, bvec] = get_jacobian_1d(ele_coords)
- 
-    jacobi_mat = (ele_coords(2) - ele_coords(1)) / 2;
-    jacobian = det(jacobi_mat);
-    bvec = (ele_coords(2) + ele_coords(1)) / 2;
-    
-end
