3272e114d67c031c20c792c9200c1af26786857f
June 雲如 <sy1162@nyu.edu>
2021-04-29 12:03:37 +0200

    Delete fem1d directory

--- fem1d/exact_data_1d.m
+++
@@ -1,5 +0,0 @@
-function exact = exact_data_1d(x)
-
-    exact = sin(x); % sin(pi * x);
-
-end
--- fem1d/source_data_1d.m
+++
@@ -1,5 +0,0 @@
-function source = source_data_1d(x)
-
-    source = sin(x); % pi ^ 2 * sin(pi * x);
-
-end
